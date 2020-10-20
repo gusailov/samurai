3d396559769f58a00606de2e3d2aa131eaac7539
gusailov <gusailov@gmail.com>
2020-10-20 16:58:40 +0300

    43

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import * as serviceWorker from './serviceWorker';
-import store from "./redux/state";
+import store from "./redux/redux-store";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -7,7 +7,7 @@ import App from './App';
 import {BrowserRouter} from "react-router-dom";
 
 let rerenderEntireTree = (state) => {
-    ReactDOM.render(
+      ReactDOM.render(
         <BrowserRouter>
             <App state={state} dispatch={store.dispatch.bind(store)} store={store} />
         </BrowserRouter>, document.getElementById('root'));
@@ -15,7 +15,10 @@ let rerenderEntireTree = (state) => {
 
 rerenderEntireTree(store.getState());
 
-store.subscribe(rerenderEntireTree);
+store.subscribe(() => {
+    let state = store.getState();
+    rerenderEntireTree(state);
+});
 
 
 // API
