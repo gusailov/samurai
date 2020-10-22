cb6bcc42aeb38ab4962a5f2fd56905a7c4eeb702
Gusailov <gusailov@gmail.com>
2020-10-22 23:03:33 +0300

    54

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './users.module.css';
+<<<<<<< Updated upstream
 
 let Users = (props) => {
     if (props.users.length === 0) {
@@ -38,18 +39,47 @@ let Users = (props) => {
                 <span>
                     <div>
                         <img src={u.photoUrl} className={styles.userPhoto}/>
+=======
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/user.png';
+
+
+class Users extends React.Component {
+    componentDidMount() {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                this.props.setUsers(response.data.items);
+            });
+    }
+
+    render() {
+        return <div>
+            {
+                this.props.users.map(u => <div key={u.id}>
+                <span>
+                    <div>
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
+>>>>>>> Stashed changes
                     </div>
                     <div>
                         {u.followed
                             ? <button onClick={() => {
+<<<<<<< Updated upstream
                                 props.unfollow(u.id)
                             }}>Unfollow</button>
                             : <button onClick={() => {
                                 props.follow(u.id)
+=======
+                                this.props.unfollow(u.id)
+                            }}>Unfollow</button>
+                            : <button onClick={() => {
+                                this.props.follow(u.id)
+>>>>>>> Stashed changes
                             }}>Follow</button>}
 
                     </div>
                 </span>
+<<<<<<< Updated upstream
                 <span>
                     <span>
                         <div>{u.fullName}</div>
@@ -65,4 +95,23 @@ let Users = (props) => {
     </div>
 }
 
+=======
+                    <span>
+                    <span>
+                        <div>{u.name}</div>
+                        <div>{u.status}</div>
+                    </span>
+                    <span>
+                        <div>{"u.location.country"}</div>
+                        <div>{"u.location.city"}</div>
+                    </span>
+                </span>
+                </div>)
+            }
+        </div>
+    }
+}
+
+
+>>>>>>> Stashed changes
 export default Users;
