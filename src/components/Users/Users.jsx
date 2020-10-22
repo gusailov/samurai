b26d28f08f2444d6fd17a454caaa8494f4c86ee4
Gusailov <gusailov@gmail.com>
2020-10-22 23:12:42 +0300

    54

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,45 +1,5 @@
 import React from 'react';
 import styles from './users.module.css';
-<<<<<<< Updated upstream
-
-let Users = (props) => {
-    if (props.users.length === 0) {
-        props.setUsers([
-                {
-                    id: 1,
-                    photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/220px-Dmitry_Nagiev_2017_4.jpg',
-                    followed: false,
-                    fullName: 'Dmitry',
-                    status: 'I am a boss',
-                    location: {city: 'Minsk', country: 'Belarus'}
-                },
-                {
-                    id: 2,
-                    photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/220px-Dmitry_Nagiev_2017_4.jpg',
-                    followed: true,
-                    fullName: 'Sasha',
-                    status: 'I am a boss too',
-                    location: {city: 'Moscow', country: 'Russia'}
-                },
-                {
-                    id: 3,
-                    photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/220px-Dmitry_Nagiev_2017_4.jpg',
-                    followed: false,
-                    fullName: 'Andrew',
-                    status: 'I am a boss too',
-                    location: {city: 'Kiev', country: 'Ukraine'}
-                }
-            ]
-        )
-    }
-
-    return <div>
-        {
-            props.users.map(u => <div key={u.id}>
-                <span>
-                    <div>
-                        <img src={u.photoUrl} className={styles.userPhoto}/>
-=======
 import * as axios from 'axios';
 import userPhoto from '../../assets/images/user.png';
 
@@ -59,43 +19,18 @@ class Users extends React.Component {
                 <span>
                     <div>
                         <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
->>>>>>> Stashed changes
                     </div>
                     <div>
                         {u.followed
                             ? <button onClick={() => {
-<<<<<<< Updated upstream
-                                props.unfollow(u.id)
-                            }}>Unfollow</button>
-                            : <button onClick={() => {
-                                props.follow(u.id)
-=======
                                 this.props.unfollow(u.id)
                             }}>Unfollow</button>
                             : <button onClick={() => {
                                 this.props.follow(u.id)
->>>>>>> Stashed changes
                             }}>Follow</button>}
 
                     </div>
                 </span>
-<<<<<<< Updated upstream
-                <span>
-                    <span>
-                        <div>{u.fullName}</div>
-                        <div>{u.status}</div>
-                    </span>
-                    <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
-                    </span>
-                </span>
-            </div>)
-        }
-    </div>
-}
-
-=======
                     <span>
                     <span>
                         <div>{u.name}</div>
@@ -113,5 +48,4 @@ class Users extends React.Component {
 }
 
 
->>>>>>> Stashed changes
 export default Users;
