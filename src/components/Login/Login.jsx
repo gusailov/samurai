47bd6a5969b20ada1fd6e9a6a0995163711bd542
gusailov <gusailov@gmail.com>
2020-10-30 15:39:49 +0200

    76

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,33 +1,36 @@
-import  React  from 'react';
-import { Field, reduxForm } from 'redux-form';
+import React from 'react';
+import {Field, reduxForm} from "redux-form";
 
 const LoginForm = (props) => {
-return <> 
-
-<form >
-<div>
-<div> < Field name={"login"} placeholder = {'Login'} component={'input'} /></div>
-<div> <Field name={"password"} placeholder = {'Password'} component={'input'}/></div>
-<div> <Field name={"rememberMe"} component={'input'} type = {'checkbox'}/>Remember me</div>
-<div> <button>Login</button> </div>
-</div>
-    
-</form>
-
-</>
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field placeholder={"Login"} name={"login"} component={"input"}/>
+            </div>
+            <div>
+                <Field placeholder={"Password"} name={"password"} component={"input"}/>
+            </div>
+            <div>
+                <Field component={"input"} name={"rememberMe"} type={"checkbox"}/> remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    )
 }
+
+const LoginReduxForm =  reduxForm({form: 'login'})(LoginForm)
+
 const Login = (props) => {
-    return(
-    <>
-    <h1>  Login  </h1>
-    <LogintReduxForm/>
-    </>)
+    const onSubmit = (formData) => {
+        console.log(formData);
+    }
+
+    return <div>
+        <h1>Login</h1>
+        <LoginReduxForm onSubmit={onSubmit} />
+    </div>
 }
 
-
-const LogintReduxForm = reduxForm ({
-    // a unique name for the form
-    form: 'login'
-  })(LoginForm)
-
- export default Login;
+export default Login;
--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,7 +3,7 @@ import Profile from "./Profile";
 import * as axios from "axios";
 import {connect} from "react-redux";
 import {getStatus, getUserProfile, updateStatus} from "../../redux/profile-reducer";
-import {withRouter} from "react-router-dom";
+import {Redirect, withRouter} from "react-router-dom";
 import {withAuthRedirect} from "../../hoc/withAuthRedirect";
 import {compose} from "redux";
 
